6d578e2a76db0f9e58cae9b10c9f3d2742b2c682
BillyClassTime <billy@billyclasstime.com>
2023-05-04 09:34:52 +0200

    Ejercicios 2 con arrays de string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+
 // initialize variables - graded assignments 
 int currentAssignments = 5;
 
@@ -7,20 +8,43 @@ int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-decimal sophiaScore;
+int[] studentScores = new int[10];
 
-foreach (int score in sophiaScores)
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
 {
-    // add the exam score to the sum
-    sophiaSum += score;
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-WriteLine("Student\t\tGrade\n");
-WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-
-Write("Press the Enter key to continue");
-ReadLine();
