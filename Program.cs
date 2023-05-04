0129818d7507bafd9ef6a093b619746e30583c74
BillyClassTime <billy@billyclasstime.com>
2023-05-04 09:32:30 +0200

    Ejercicio 1 de arrays y foreach

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+int sophiaSum = 0;
+
+decimal sophiaScore;
+
+foreach (int score in sophiaScores)
+{
+    // add the exam score to the sum
+    sophiaSum += score;
+}
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+WriteLine("Student\t\tGrade\n");
+WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+Write("Press the Enter key to continue");
+ReadLine();
